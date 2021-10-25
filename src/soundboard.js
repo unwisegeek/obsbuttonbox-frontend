58f2b5f1ff8ffe794a327d185b14b720c6cdbfb4
John Madon <john@unwisegeek.net>
2021-10-25 18:36:20 -0400

    Added sounds_available test call

--- src/soundboard.js
+++ src/soundboard.js
@@ -11,6 +11,9 @@ const config = require('./config.js');
 var referrer = window.location.href;
 var api = `http://${config["api_host"]}:${config["api_port"]}`;
 
+let sounds_available = fetch(`${api}/api/sounds-available`)
+print(sounds_available)
+
 function createButtonData(label, link) {
   return { label, link };
 }
